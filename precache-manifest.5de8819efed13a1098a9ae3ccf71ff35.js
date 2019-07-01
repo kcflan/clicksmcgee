b77f9f940a2b83b7c8ee3a3a4d32b70d4fe340d1
Kevin Flanagan <kflan3000@gmail.com>
2019-06-30 20:25:17 -0700

    Updates

--- precache-manifest.5de8819efed13a1098a9ae3ccf71ff35.js
+++ precache-manifest.5de8819efed13a1098a9ae3ccf71ff35.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b576418e9a9055f50c8fb0bb3835a60",
+    "revision": "fc3c6130df92d26ee0acc4f569677d56",
     "url": "/clicksmcgee/index.html"
   },
   {
-    "revision": "84f79b923918a5065ddb",
-    "url": "/clicksmcgee/static/css/main.2e1cfe92.chunk.css"
+    "revision": "cb569db20cf9f6141a8d",
+    "url": "/clicksmcgee/static/css/main.247e58c0.chunk.css"
   },
   {
     "revision": "dd0ed99a380f8d225736",
     "url": "/clicksmcgee/static/js/2.884c28c3.chunk.js"
   },
   {
-    "revision": "84f79b923918a5065ddb",
-    "url": "/clicksmcgee/static/js/main.e9771ad9.chunk.js"
+    "revision": "cb569db20cf9f6141a8d",
+    "url": "/clicksmcgee/static/js/main.438d302c.chunk.js"
   },
   {
     "revision": "0eed7e97ee994db51fbf",
