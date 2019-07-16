49c833353e47b6f54148106038a275f83b168c89
Kevin Flanagan <kflan3000@gmail.com>
2019-07-15 18:44:22 -0700

    code base, non static

---
+++ build/precache-manifest.5de8819efed13a1098a9ae3ccf71ff35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3c6130df92d26ee0acc4f569677d56",
+    "url": "/clicksmcgee/index.html"
+  },
+  {
+    "revision": "cb569db20cf9f6141a8d",
+    "url": "/clicksmcgee/static/css/main.247e58c0.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicksmcgee/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "cb569db20cf9f6141a8d",
+    "url": "/clicksmcgee/static/js/main.438d302c.chunk.js"
+  },
+  {
+    "revision": "0eed7e97ee994db51fbf",
+    "url": "/clicksmcgee/static/js/runtime~main.4edea8d1.js"
+  }
+]);
